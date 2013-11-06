fbe39a038b8112649eba87b35d63d7c562447085
Irene <iruuunechka@gmail.com>
2013-11-06 12:15:21 +0400

    For all groups tests are generated simultaneously

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,37 @@ import java.io.*;
 
 
 public class Main {
-
-
+    public static void createFiles(File dir, String tasksFile, String taskDir, String outDir, PrintWriter pw) {
+        for (File group : dir.listFiles()) {
+            try {
+                TaskMaker tm = new TaskMaker(group.getPath(), tasksFile, taskDir);
+                //tm.print(outFile);
+                int dotPos = group.getName().lastIndexOf('.');
+                tm.createAllVariants(outDir + "/test" + group.getName().substring(dotPos - 4, dotPos) + ".tex");
+                if (pw != null) {
+                    pw.println("pdflatex " +  "test" + group.getName().substring(dotPos - 4, dotPos) + ".tex");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        String studentsFile = args[0];
+        String studentsFolder = args[0];
         String tasksFile = args[1];
-        String outFile = args[2];
+        String outDir = args[2];
         String taskDir = args[3];
+
+        File dir = new File(studentsFolder);
+
+        new File(outDir).mkdirs();
+
         try {
-            TaskMaker tm = new TaskMaker(studentsFile, tasksFile, taskDir);
-            //tm.print(outFile);
-            tm.createAllVariants(outFile);
-        } catch (IOException e) {
+            PrintWriter pw = new PrintWriter(new File(outDir + "/r.sh"));
+            createFiles(dir, tasksFile, taskDir, outDir, pw);
+            pw.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
